6f2864cf83bd474972347bc55158ed5fd1b71ba7
Abiola Olamilekan <abiolawalter2015@gmail.com>
2023-11-11 14:10:23 +0100

    feat: setup dark & light mode with redux

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
+import * as React from 'react';
 
-cosnt App = () => {
+import { CssBaseline, ThemeProvider } from '@mui/material';
+import { createTheme } from '@mui/material/styles';
+import { useSelector } from 'react-redux';
+import { themeSettings } from 'theme';
+
+const App = () => {
+  const mode = useSelector((state) => state.global.mode);
+  const theme = React.useMemo(() => createTheme(themeSettings(mode)), [mode]);
   return (
-    <div className="app">
-
+    <div className='app'>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+      </ThemeProvider>
     </div>
   );
-}
+};
 
 export default App;
